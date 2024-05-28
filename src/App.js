c5b66199f23a135deb0d2347b49d5d605994382c
鄭采妮 <184071@cch.org.tw>
2024-05-28 15:45:31 +0800

    1212

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Select  from 'react-select'
 
 function App() {
+  const qryKinds=[{value:'stockCode',label:'代碼'},{value:'stockName',label:'名稱'}];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display:"inline-flex", width: "90%" }}>
+        <div style={{ }}>
+          <div style={{ display: "inline-flex",marginTop:'20px' }}>
+            <Select options={qryKinds} />
+            <input type="text" styles={{flex:'1'}}/>
+            <button style={{width:'70px'}}>查詢</button>
+          </div>
+          <table style={{margin:'10px',width:'90%',border:'solid 1px blue'}}>
+            <tr>
+              <th style={{width:'20%',border:'solid 1px blue'}}>代碼</th>
+              <th style={{width:'60%',border:'solid 1px blue'}}>名稱</th>
+              <th style={{width:'20%',border:'solid 1px blue'}}></th>
+            </tr>
+          </table>
+        </div>
+        <div style={{ background: "pink",flex:"1",height:"100px" }}></div>
+      </div>
     </div>
   );
 }
